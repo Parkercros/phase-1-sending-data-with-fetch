275557414c481476e64b37ca575a7aee5bc0a849
Parker Crosgrove <pdxparker3@gmail.com>
2023-02-06 14:12:25 -0700

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: "Steve",
+      email: "steve@steve.com",
+    };
+    
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(formData)
+  };
+  fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Error!");
+    console.log(error.message);
+  });
+}
+
+
